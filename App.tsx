01301c0498589b89bf2b3817be1de3b3d11703c4
Henrique Cícero <henriquecicero@gmail.com>
2025-12-31 20:35:12 -0300

    refactor: simplify App component and move logic to samplemoduleview

--- App.tsx
+++ App.tsx
@@ -1,50 +1,14 @@
 import React from 'react';
-import {
-  Button,
-  ScrollView,
-  StyleSheet,
-  Text,
-  TextInput,
-  View,
-} from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 import { CounterView } from './components/CounterView';
 import Divider from './components/Divider';
 import LocalStorageView from './components/LocalStorageView';
 import PDFView from './components/PDFView';
-import NativeSampleModule from './specs/NativeSampleModule';
+import SampleModuleView from './components/SampleModuleView';
 
 function App(): React.JSX.Element {
-  const [value, setValue] = React.useState('');
-  const [reversedValue, setReversedValue] = React.useState('');
-  const [cubicSource, setCubicSource] = React.useState('');
-  const [cubicRoot, setCubicRoot] = React.useState(0);
-  const [street, setStreet] = React.useState('');
-  const [num, setNum] = React.useState('');
-  const [isValidAddress, setIsValidAddress] = React.useState<boolean | null>(
-    null,
-  );
-
-  const onPressValidateAddress = () => {
-    let houseNum = parseInt(num, 10);
-    if (isNaN(houseNum)) {
-      houseNum = -1;
-    }
-    const address = {
-      street,
-      num: houseNum,
-      isInUS: false,
-    };
-    const result = NativeSampleModule.validateAddress(address);
-    setIsValidAddress(result);
-  };
-
-  const onPress = () => {
-    const revString = NativeSampleModule.reverseString(value);
-    setReversedValue(revString);
-  };
-
   return (
     <SafeAreaProvider>
       <SafeAreaView style={styles.container}>
@@ -56,51 +20,7 @@ function App(): React.JSX.Element {
             <Text style={styles.title}>
               Welcome to Turbo Native Modules Example
             </Text>
-            <Text>Write down here the text you want to reverse</Text>
-            <TextInput
-              style={styles.textInput}
-              placeholder="Write your text here"
-              onChangeText={setValue}
-              value={value}
-            />
-            <Button title="Reverse" onPress={onPress} />
-            <Text>Reversed text: {reversedValue}</Text>
-            <Divider size={24} />
-            <Text>For which number do you want to compute the Cubic Root?</Text>
-            <TextInput
-              style={styles.textInput}
-              placeholder="Write your text here"
-              onChangeText={setCubicSource}
-              value={cubicSource}
-            />
-            <Button
-              title="Get Cubic Root"
-              onPress={() =>
-                setCubicRoot(NativeSampleModule.cubicRoot(cubicSource))
-              }
-            />
-            <Text>The cubic root is: {cubicRoot}</Text>
-            <Divider size={24} />
-            <Text>Address:</Text>
-            <TextInput
-              style={styles.textInput}
-              placeholder="Write your address here"
-              onChangeText={setStreet}
-              value={street}
-            />
-            <Text>Number:</Text>
-            <TextInput
-              style={styles.textInput}
-              placeholder="Write your address here"
-              onChangeText={setNum}
-              value={num}
-            />
-            <Button title="Validate" onPress={onPressValidateAddress} />
-            {isValidAddress != null && (
-              <Text>
-                Your address is {isValidAddress ? 'valid' : 'not valid'}
-              </Text>
-            )}
+            <SampleModuleView />
           </View>
           <Divider size={24} />
           <LocalStorageView />
@@ -125,14 +45,6 @@ const styles = StyleSheet.create({
     fontSize: 18,
     marginBottom: 20,
     textAlign: 'center',
-  },
-  textInput: {
-    borderColor: 'black',
-    borderWidth: 1,
-    borderRadius: 5,
-    padding: 10,
-    marginTop: 10,
-    marginBottom: 10,
   },
 });
 
